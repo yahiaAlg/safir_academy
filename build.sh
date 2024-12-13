7ec91ef0aebb49b5dc50fa3f0691a2be3602b783
yahiaAlg <yahialinus21alg@@gmail.com>
2024-12-13 10:38:48 +0100

    `Update build.sh to install additional dependencies and tools`

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+apt-get update
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install -y zbar-tools libzbar0 libzbar-dev
 # Install Python dependencies
 pip install -r requirements.txt
 
